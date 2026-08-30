9ee96d51dbf73d49ed84f409a2c717ea03c6564a
salerno00 <gslernoambrosio00@gmail.com>
2026-08-30 13:57:26 -0300

    adicionar pastas e arquivos para projeto

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<header class="header">
+
+    <div class="logo">
+        <img src="img/logo-maze-removebg-preview.png" alt="Logo da empresa">
+    </div>
+
+    <nav class="menu">
+        <a href="index.php">Início</a>
+        <a href="#">Produtos</a>
+        <a href="#">Coleções</a>
+        <a href="#">Sobre nós</a>
+        <a href="#">Contato</a>
+    </nav>
+
+
+</header>
