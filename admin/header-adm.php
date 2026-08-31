e7bf15dd9f3c3fa6f231ddf55dfd275d2fa12b10
Guizanato <guimzntt@gmail.com>
2026-08-31 10:25:22 -0300

    adicionar header da parte administrativa

---
+++ admin/header-adm.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="/maze-streetwear/assets/style.css">
+</head>
+
+<body>
+
+<header class="header">
+
+    <div class="logo">
+        <img src="/maze-streetwear/img/logo-maze-removebg-preview.png" alt="Logo da empresa">
+    </div>
+
+    <nav class="menu">
+        <a href="/maze-streetwear/index.php">Retornar ao site</a>
+    </nav>
+
+</header>
